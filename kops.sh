22147697ff91bb52a86777c146bb8fd29ba84b2d
Riyaz-ops <riyaz190920@gmail.com>
2025-06-16 08:15:46 +0530

    Updated kops.sh

--- kops.sh
+++ kops.sh
@@ -2,17 +2,18 @@
 #export PATH=$PATH:/usr/local/bin/
 #source .bashrc
 
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
-#! /bin/bash
-aws configure
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-wget https://github.com/kubernetes/kops/releases/download/v1.25.0/kops-linux-amd64
-chmod +x kops-linux-amd64 kubectl
-mv kubectl /usr/local/bin/kubectl
-mv kops-linux-amd64 /usr/local/bin/kops
+chmod +x kubectl
+mv kubectl /usr/local/bin/
 
-aws s3api create-bucket --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://cloudanddevopsbyraham0073456.k8s.local
-kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
-kops update cluster --name rahams.k8s.local --yes --admin
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+mv kops /usr/local/bin/kops
+
+export KOPS_STATE_STORE=s3://riyaz.flm.k8s
+kops create cluster --name kuber.k8s.local --zones us-east-1a,us-east-1b --master-count=1 --master-size t2.medium --master-volume-size 30 --node-count=2 --node-size t2.medium --node-volume-size 30
+kops update cluster --name kuber.k8s.local --yes --admin
